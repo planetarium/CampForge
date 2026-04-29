31d2910a9888c32e0b4f400334f24ea8b51dbb0a
Swen Mun <longfinfunnel@gmail.com>
2026-04-30 00:21:47 +0900

    fix: align flex-ax install metadata

--- camps/flex-ax-remote/install.sh
+++ camps/flex-ax-remote/install.sh
@@ -9,17 +9,6 @@ BASE="https://github.com/planetarium/CampForge/releases/download/flex-ax-remote-
 WS="${WORKSPACE:-workspace}"
 mkdir -p "$WS" && cd "$WS"
 
-npm init -y --silent 2>/dev/null
-npm pkg set \
-  "dependencies.@campforge/a2x=$BASE/campforge-a2x-0.1.0.tgz" \
-  "dependencies.@campforge/gql-ops=$BASE/campforge-gql-ops-0.2.0.tgz" \
-  "dependencies.@campforge/gws-auth=$BASE/campforge-gws-auth-0.1.0.tgz" \
-  "dependencies.@campforge/gws-sheets=$BASE/campforge-gws-sheets-0.1.1.tgz" \
-  "dependencies.@campforge/gws-gmail=$BASE/campforge-gws-gmail-0.1.1.tgz" \
-  "dependencies.@campforge/gws-drive=$BASE/campforge-gws-drive-0.1.1.tgz"
-
-npx skillpm install
-
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")" && pwd)"
 COMMON="$SCRIPT_DIR/../../scripts/install-common.sh"
 if [ -f "$COMMON" ]; then
@@ -32,6 +21,19 @@ else
   source "$TMP_COMMON"
   rm -f "$TMP_COMMON"
 fi
+
+ensure_node_runtime
+
+npm init -y --silent 2>/dev/null
+npm pkg set \
+  "dependencies.@campforge/a2x=$BASE/campforge-a2x-0.1.0.tgz" \
+  "dependencies.@campforge/gql-ops=$BASE/campforge-gql-ops-0.2.0.tgz" \
+  "dependencies.@campforge/gws-auth=$BASE/campforge-gws-auth-0.1.0.tgz" \
+  "dependencies.@campforge/gws-sheets=$BASE/campforge-gws-sheets-0.1.1.tgz" \
+  "dependencies.@campforge/gws-gmail=$BASE/campforge-gws-gmail-0.1.1.tgz" \
+  "dependencies.@campforge/gws-drive=$BASE/campforge-gws-drive-0.1.1.tgz"
+
+npx skillpm install
 
 install_a2x
 install_gws
